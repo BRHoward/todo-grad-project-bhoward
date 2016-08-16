f98df03cbb49628711eafe74041350dc79623ad8
Ben Howard <bhoward@scottlogic.co.uk>
2016-08-16 08:40:34 +0100

    added input field for updating todos

--- public/main.js
+++ public/main.js
@@ -32,7 +32,7 @@ function createTodo(title, callback) {
 function removeTodo(id, callback) {
     var createRequest = new XMLHttpRequest();
     createRequest.open("DELETE", "/api/todo/" + id);
-    createRequest.onload = function () {
+    createRequest.onload = function() {
         if (this.status === 200) {
             callback();
         } else {
@@ -63,28 +63,43 @@ function reloadTodoList() {
     getTodoList(function(todos) {
         todoListPlaceholder.style.display = "none";
         todos.forEach(function(todo) {
-            var listItem = document.createElement("li");
-            listItem.textContent = todo.title;
-            var delBtn = document.createElement("button");
-            delBtn.className = "del-btn";
-            delBtn.id = "del-btn" + todo.id;
-            delBtn.onclick = function() {
-                removeTodo(todo.id, reloadTodoList);
-            };
-            delBtn.innerHTML = "Delete";
-            var updBtn = document.createElement("button");
-            updBtn.className = "upd-btn";
-            updBtn.id = "upd-btn" + todo.id;
-            updBtn.onclick = function() {
-                //update the todo and reload the list
-                updateTodo();
-            };
-            updBtn.innerHTML = "Update";
-            listItem.appendChild(delBtn);
-            listItem.appendChild(updBtn);
-            todoList.appendChild(listItem);
+            todoList.appendChild(generateTodoListElement(todo));
         });
     });
+}
+
+function generateTodoListElement(todo) {
+    var listItem = document.createElement("li");
+    var todoText = document.createElement("span");
+    todoText.textContent = todo.title;
+    listItem.appendChild(todoText);
+    //listItem.textContent = todo.title;
+    var delBtn = document.createElement("button");
+    delBtn.className = "del-btn";
+    delBtn.id = "del-btn" + todo.id;
+    delBtn.onclick = function() {
+        //remove the 
+        removeTodo(todo.id, reloadTodoList);
+    };
+    delBtn.innerHTML = "Delete";
+    var updBtn = document.createElement("button");
+    updBtn.className = "upd-btn";
+    updBtn.id = "upd-btn" + todo.id;
+    updBtn.innerHTML = "Update";
+    updBtn.onclick = function() {
+        //create input field for updating the todo
+        var updInput = document.createElement("input");
+        updInput.setAttribute("type","text");
+        updInput.setAttribute("value", todo.title);
+        todoText.parentNode.replaceChild(updInput, todoText);
+        updBtn.innerHTML = "Confirm";
+        updBtn.onclick = function() {
+            updateTodo(todo.id);
+        }
+    };
+    listItem.appendChild(delBtn);
+    listItem.appendChild(updBtn);
+    return listItem;
 }
 
 reloadTodoList();
